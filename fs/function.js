2749a34c8202cc709b5f85f0fcfef833f23b3a4c
kscrap <kevincs1987@gmail.com>
2017-06-04 20:03:17 -0400

    working on new fs site

---
+++ fs/function.js
@@ -0,0 +1,33 @@
+function displayGreeting()
+{
+	document.write("Hey there! What's your name?");
+}
+
+function displayPlayerScore(theScore)
+{
+	//playerScore = 1000;
+	theScore += 1000; //theScore = theScore + 1000;
+	document.write("<br/>Player score: " + theScore);
+}
+
+function addTheseNumbers(x,y)
+{
+	return (x+y);
+	
+}
+
+function onButtonClick()
+{
+	alert("You have successfully pressed the button!");
+	
+	
+	
+}
+
+function whenPageLoads()
+{
+	alert("Welcome to my page");
+	
+	
+	
+}
